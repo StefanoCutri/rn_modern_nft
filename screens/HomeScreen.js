768bbd16c62fa5ab7b0ae9d8c637071b12f8f7c9
Stefano Cutri <stefanocutristecco@gmail.com>
2023-04-06 01:34:14 -0300

    Home screen styled

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, FlatList } from "react-native";
+import React, { useState } from "react";
+import { COLORS, NFTData } from "../constants";
+import { FocusedStatusBar, HomeHeader, NFTCard } from "../components/index";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, background: COLORS.primary }} />
+          <View style={{ flex: 1, background: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
